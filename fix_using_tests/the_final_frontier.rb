198530a1b13997fd45b643cb0afb29710ba53f57
Stephen Peters <7speter@gmail.com>
2020-05-07 15:58:03 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -13,11 +13,9 @@ def state_log(star_date)
 end
 
 def engage
-  #date = generate_star_date
-  binding.pry
-  puts state_log(date)
   date = generate_star_date
-  binding.pry
-end
+  #binding.pry
+  puts state_log(date)
+  #date = generate_star_date
 
-engage()
+end
