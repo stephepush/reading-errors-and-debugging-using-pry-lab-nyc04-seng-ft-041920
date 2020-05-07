3d2373556d57adacce27b741232f697934627e4e
Stephen Peters <7speter@gmail.com>
2020-05-07 15:31:38 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,7 +1,12 @@
 # don't forget to add: require 'pry'
+require 'pry'
+
 
 def get_user_input
-  gets.chomp
+  #gets.chomp
+  num = gets.chomp
+  #binding.pry
+  num
 end
 
 def prompt_user
@@ -11,14 +16,16 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
+  num
 end
 
 def runner
   prompt_user
-  selection(get_user_input)
+  puts selection(get_user_input)
+
 end
--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,18 @@
 # don't forget to add: require 'pry'
+require 'pry'
+
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{player2}"
+  #binding.pry
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   "#{player1} is better than #{player2}"
+  #binding.pry
 end
+
+player1 = "Stephen"
+player2 = "David"
+start_game(player1, player2)
+play_game(player1, player2)
